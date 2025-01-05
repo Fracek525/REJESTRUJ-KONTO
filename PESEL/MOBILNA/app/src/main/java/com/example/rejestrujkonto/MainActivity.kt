9a7d0390f6599bf3244171ff159f2d5cf0adfbdf
Fracek525 <michal.frackiewicz@cyfroweszkoly.pl>
2025-01-06 00:43:43 +0100

    Stworzenie funkcjonalności aplikacji mobilnej

--- PESEL/MOBILNA/app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
+++ PESEL/MOBILNA/app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -30,7 +31,17 @@ class MainActivity : AppCompatActivity() {
 
 
         confirm.setOnClickListener {
-
+            if (email.text.toString().contains("@")) {
+                if (password.text.toString() == samePassword.text.toString()) {
+                    emailText.text = "Witaj ${email.text.toString()}"
+                }
+                else {
+                    emailText.text = "Hasła się różnią"
+                }
+            }
+            else {
+                emailText.text = "Nieprawidłowy adres e-mail"
+            }
         }
     }
 }
