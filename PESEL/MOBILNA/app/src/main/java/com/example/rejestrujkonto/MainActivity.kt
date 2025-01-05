c22149bec8b8021c8423169f0db54935194ae2dc
Fracek525 <michal.frackiewicz@cyfroweszkoly.pl>
2025-01-06 00:28:29 +0100

    Stworzenie pełnego modelu wyglądu w aplikacji mobilnej

--- PESEL/MOBILNA/app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
+++ PESEL/MOBILNA/app/src/main/java/com/example/rejestrujkonto/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.rejestrujkonto
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,17 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.password)
+        val samePassword: EditText = findViewById(R.id.samePassword)
+        val confirm: Button = findViewById(R.id.confirm)
+        val emailText: TextView = findViewById(R.id.emailText)
+
+
+        confirm.setOnClickListener {
+
         }
     }
 }
